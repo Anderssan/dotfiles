47ec8c674293341d63c1e6d366a8676fc2d5a147
Sebastian Lyng Johansen <seb.l.j@hotmail.com>
2019-10-17 22:26:54 +0200

    added script for creating symlinks to dotfiles, install oh-my-zsh and setting shell to zsh as default

---
+++ symlink_script.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+#Creates symlink to files inside dotfiles-repo
+#Use -sfn instead of -s to overwrite existing dotfiles
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.zshrc ~/.zshrc 
+
+install_zsh () {
+# Test to see if zshell is installed.  If it is:
+if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+    if [[ ! -d ~/.oh-my-zsh ]] || [[ ! -d ~/oh-my-zsh ]]; then
+		cd && git clone http://github.com/robbyrussell/oh-my-zsh.git
+    fi
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+        chsh -s $(which zsh)
+    fi
+else
+    # If zsh isn't installed, get the platform of the current machine
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install zsh and then recurse
+    if [[ $platform == 'Linux' ]]; then
+        if [[ -f /etc/redhat-release ]]; then
+            sudo yum install zsh
+            install_zsh
+        fi
+        if [[ -f /etc/debian_version ]]; then
+            sudo apt-get install zsh
+            install_zsh
+        fi
+    # If the platform is OS X, tell the user to install zsh :)
+    elif [[ $platform == 'Darwin' ]]; then
+        echo "Please install zsh, then re-run this script!"
+        exit
+    fi
+fi
+}
+
+install_zsh
+
